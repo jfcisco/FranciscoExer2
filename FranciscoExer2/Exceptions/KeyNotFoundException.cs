d0b22a47e7ed458ef9c0bdc3aa88df4d6e8a8552
Jasper Francisco <jasper.francisco@gmail.com>
2021-04-22 21:39:40 +0800

    Add KeyNotFound and SuccessorNotFound exceptions

---
+++ FranciscoExer2/Exceptions/KeyNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FranciscoExer2.Exceptions
+{
+    /// <summary>
+    /// Exception for when the user tries to search for a given key that is not yet inserted in the BST.
+    /// </summary>
+    public class KeyNotFoundException : Exception { }
+}
---
+++ FranciscoExer2/Exceptions/SuccessorNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FranciscoExer2.Exceptions
+{
+    /// <summary>
+    /// Exception for when the user tries to find the successor of a maximum node (i.e, a node with no successor).
+    /// </summary>
+    public class SuccessorNotFoundException : Exception { }
+}
