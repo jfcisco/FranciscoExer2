3f3e646e2b42efa6bdb10b91c8cac3c5cc0a584b
Jasper Francisco <jasper.francisco@gmail.com>
2021-04-22 20:18:30 +0800

    Add traversal for Delete method, rename Head to Root

--- FranciscoExer2/BinarySearchTree.cs
+++ FranciscoExer2/BinarySearchTree.cs
@@ -7,15 +7,15 @@ namespace FranciscoExer2
     // My implementation of the Binary Search Tree (BST) data structure.
     public class BinarySearchTree
     {
-        private Node Head;
+        private Node Root;
 
-        // Explicit default constructor
+        // Declares an empty tree
         public BinarySearchTree() { }
 
         // Inserts a given value into the BST, maintaining the BST ordering property. The public interface to the recursive Insert method.
         public void Insert(int value)
         {
-            Insert(ref Head, value);
+            Insert(ref Root, value);
         }
 
         // Method overload to insert the value to the BST, recursively.
@@ -41,10 +41,10 @@ namespace FranciscoExer2
         public int Maximum()
         {
             // Handle scenario when BST is empty.
-            if (Head == null) throw new Exception("Error: BST is empty.");
+            if (Root == null) throw new Exception("Error: BST is empty.");
 
             // Look for the maximum value iteratively.
-            Node current = Head;
+            Node current = Root;
 
             while (current.Right != null)
             {
@@ -58,9 +58,9 @@ namespace FranciscoExer2
         public string Print()
         {
             string toPrint = "";
-            if (Head != null)
+            if (Root != null)
             {
-                Print(ref toPrint, Head);
+                Print(ref toPrint, Root);
             }
 
             // Remove the last comma at the end of the string
@@ -89,9 +89,39 @@ namespace FranciscoExer2
             }
         }
 
+        // Deletes the node with a key matching the given value.
         public void Delete(int value)
         {
-            throw new NotImplementedException();
+            // Update the Root node
+            Root = Delete(Root, value);
+        }
+
+        // Private recursive delete method
+        // Returns the reference to the node or the new node took its place
+        private Node Delete(Node node, int value)
+        {
+            // Node is null if either the value cannot be found or the tree is empty.
+            if (node == null) { return null; }
+
+            if (value == node.Key)
+            {
+                // Delete the node considering the number of its children
+                return null;
+            }
+            else if (value < node.Key)
+            {
+                // Traverse the node's left subtree to find the node to be deleted.
+                // Update the node's left child.
+                node.Left = Delete(node.Left, value);
+                return node;
+            }
+            else // if value > node.Key
+            {
+                // Traverse the node's left subtree to find the node to be deleted.
+                // Update the node's right child.
+                node.Right = Delete(node.Right, value);
+                return node;
+            }
         }
 
         // Nested node class to represent the nodes of the BST.
